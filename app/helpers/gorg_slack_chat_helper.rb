6acbd720a1f627dac5ca8e2bf94a513d42f7ce63
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2017-01-18 10:37:58 +0100

    Update user attributes, change rails version

--- app/helpers/gorg_slack_chat_helper.rb
+++ app/helpers/gorg_slack_chat_helper.rb
@@ -4,13 +4,13 @@ module GorgSlackChatHelper
     render partial:'shared/tags_for_gorg_slack_chat'
   end
 
-  def gorg_slack_chat
+  def gorg_slack_chat(name: nil, id: nil, link: nil )
     session[:foo] = "bar" #Initialize session if not set
 
     opts= {
-            user_name: "Utilisateur Anonyme - " + session.id[0..4].upcase,
-            user_id: "",
-            user_link: ""
+            user_name: (name||"Utilisateur Anonyme - " + session.id[0..4].upcase),
+            user_id: (id||""),
+            user_link: (link||"")
         }
 
     render partial:'shared/gorg_slack_chat', :locals => opts
